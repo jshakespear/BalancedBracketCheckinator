8ab5d8a4d41f8991f83f2d9f4cb3a53751bf9b95
Justin Shakespear <jshakespear@gmail.com>
2022-07-14 19:06:37 -0600

    initial commit

---
+++ BalancedBracketCheckinator/Parity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace BalancedBracketCheckinatorNS
+{
+    public class Parity
+    {
+        public static bool checkBracketParity(string testString)
+        {
+            const char CLOSING_BRACKET = '}';
+            const char OPENING_BRACKET = '{';
+            Stack<string> stack = new Stack<string>();
+
+            foreach (char value in testString.ToCharArray())
+            {
+                if (value.Equals(OPENING_BRACKET))
+                {
+                    stack.Push(value.ToString());
+                }
+                else if (value.Equals(CLOSING_BRACKET))
+                {
+                    if (stack.Count > 0)
+                    {
+                        string removed = stack.Pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return (stack.Count == 0);
+        }
+
+        public static void Main()
+        {
+            bool result = Parity.checkBracketParity("test");
+        }
+    }
+
+}
---
+++ BalancedBracketCheckinatorTests/ParityTests.cs
@@ -0,0 +1,87 @@
+﻿using BalancedBracketCheckinatorNS;
+
+namespace BalancedBracketCheckinatorTests;
+
+[TestClass]
+public class ParityTests
+{
+    [TestMethod]
+    public void HappyPath_BalancedBrackets()
+    {
+        bool result = Parity.checkBracketParity("{}");
+
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void BeginningClosedBracket()
+    {
+        bool result = Parity.checkBracketParity("}{");
+
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void OneUnclosedBracket()
+    {
+        bool result = Parity.checkBracketParity("{{}");
+
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void EmptyString()
+    {
+        bool result = Parity.checkBracketParity("");
+
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void NonBracketCharactersOnly()
+    {
+        bool result = Parity.checkBracketParity("abc...xyz");
+
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void BalancedBracketsWithNonBracketCharactersOnly()
+    {
+        bool result = Parity.checkBracketParity("{abc...xyz}");
+
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void BeginningClosedBracketWithNonBracketCharactersOnly()
+    {
+        bool result = Parity.checkBracketParity("}abc...xyz{");
+
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void BalancedNestedBrackets()
+    {
+        bool result = Parity.checkBracketParity("{{}{}}");
+
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void BalancedMultilevelBrackets()
+    {
+        bool result = Parity.checkBracketParity("{{{}}}");
+
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void UnbalancedMultilevelBrackets()
+    {
+        bool result = Parity.checkBracketParity("{{}}}");
+
+        Assert.IsFalse(result);
+    }
+}
